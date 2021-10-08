8006c65aa78321484c957991bced2992b74d9f56
Mick <mickx.shular@gmail.com>
2021-10-08 13:45:26 -0400

    comments, bounds script and player bounds

--- Assets/[Scripts]/PlayerMovement.cs
+++ Assets/[Scripts]/PlayerMovement.cs
@@ -6,11 +6,16 @@ using UnityEngine;
 //Version 0.1
 //-player move by adding force
 //-decay used to limit speed
+//-limits player movement inside screen 
+
+//Last edited: 10/8/2021
+
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D playerRigidbody;
     [SerializeField] float speed;
     [Range(0.0f, 1.0f)][SerializeField] float decay;
+    [SerializeField] Bounds bounds;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +26,20 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
+        //take inputs from x-axis and multiples it by speed to add forec to player 
         playerRigidbody.AddForce(new Vector2(Input.GetAxisRaw("Horizontal") * speed, 0));
+        //slow player down
         playerRigidbody.velocity *= (1.0f - decay);
+
+        //player minimum distance on x-axis 
+        if (transform.position.x < bounds.min)
+        {
+            transform.position = new Vector2(bounds.min, transform.position.y);
+        }
+        //player maximum distance on x-axis 
+        if (transform.position.x > bounds.max)
+        {
+            transform.position = new Vector2(bounds.max, transform.position.y);
+        }
     }
 }
