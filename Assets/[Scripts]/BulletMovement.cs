f45d0236bdcf22f404bd7c6fd4f8c779bf7c8cf7
Mick <mickx.shular@gmail.com>
2021-10-15 20:30:02 -0400

    bullet direction, added player bullet, added more collision to bullet movement

--- Assets/[Scripts]/BulletMovement.cs
+++ Assets/[Scripts]/BulletMovement.cs
@@ -14,18 +14,39 @@ public class BulletMovement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private Bounds bulletBounds;
     private BulletManager bulletManager;
+    private Vector3 velocity;
+    public BulletDirection direction;
     // Start is called before the first frame update
     void Start()
     {
         bulletManager = GameObject.FindObjectOfType<BulletManager>();
+
+        switch (direction)
+        {
+            case BulletDirection.UP:
+                velocity = new Vector3(0.0f, speed, 0.0f);
+                break;
+            case BulletDirection.DOWN:
+                velocity = new Vector3(0.0f, -speed, 0.0f);
+                break;
+            default:
+                break;
+             
+        }
     }
     private void FixedUpdate()
     {
         //bullet movemenmt
-        transform.position -= new Vector3(0.0f, speed, 0.0f);
+        transform.position += velocity;
         
         //move bullet back into queue if true
         if(transform.position.y < bulletBounds.min)
+        {
+            bulletManager.destoryBullet(this.gameObject);
+        }
+
+        //move bullet back into queue if true
+        if (transform.position.y > bulletBounds.max)
         {
             bulletManager.destoryBullet(this.gameObject);
         }
